c44d23d9274dc957710966020c7f9f635e1ffab6
Dinesh11112 <dineshboby07@gmail.com>
2020-10-23 09:12:46 -0400

    index,about html files modified to php, header and footer added, included header,footer in index,about pages

---
+++ footer.php
@@ -0,0 +1,19 @@
+<?php
+
+echo"
+<footer>
+<p>Subscribe to get latest updates and weekly newsletter</p>
+<input id='mail' type='text' placeholder='EMAIL ADDRESS' name='mail' required>&nbsp;&nbsp;
+<input id='subscribe' type='submit' value='SUBSCRIBE'>
+<section class='bottom'>
+  <h2>Bank Of Ontario</h2>
+  <p>At BFO, we are in business to help our clients, employees and shareholders achieve what is important to them.<br><br> Our ability to create value for all BFO stakeholders is driven by a business culture based on common values:<br><br> Trust<br> Teamwork<br> Accountability<br><br>
+    All you have to do is sign-up and start exploring!</p><br><br>
+  <a href='contact.html'>Contact Us</a>
+  <a href='faq.html'>FAQ</a>&nbsp;&nbsp;
+  <a href='terms.html'>Terms&Conditions</a>&nbsp;&nbsp;
+  <a href='privacy.html'>Privacy Policy</a>&nbsp;&nbsp;
+</section>
+</footer>
+"
+?>
---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+echo "
+<html>
+<head></head>
+<body>
+    <header>
+        <img src='images/logoB.png' alt='logo' width='50' height='50'/>
+        <h1>Bank Of Ontario</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href='index.html' target='_self'>Personal</a></li>
+            <li><a href='about_page.html' target='_self'>About</a></li>
+            <li id='login'><a href='login.php' target='_self'>LogIn</a></li>
+            <li><a href='signup.html' target='_self'>SignUp</a></li>
+        </ul>
+    </nav>
+    </div>
+"
+?>
