7a9bcd77a7c12c274fad1223f1c92af526910449
Akash Basava <saiakash81197@gmail.com>
2020-10-23 09:27:53 -0400

    faq page created

---
+++ faq.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<!-- Title for the page -->
+<title> About Bank Of Ontario : One stop for Financial Goals</title>
+<!--Meta information-->
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<meta name="description" content="This is a About Page"/>
+<meta name="keywords" content="about Bank Of Ontario"/>
+<link rel="stylesheet" href="css/about.css"/>
+<link rel="shortcut icon" href="images/logoB.png"/>
+
+</head>
+
+<body>
+    <div id="top">
+        <header>
+            <img src="images/logoB.png" alt="logo" width="50" height="50"/>
+            <h1>Bank Of Ontario</h1>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="index.html" target="_self">Personal</a></li>
+                <li><a href="business.html">Business</a></li>
+                <li><a href="commercial.html" target="_self">Commercial</a></li>
+                <li><a href="investments.html" target="_self">Investments</a></li>
+                <li><a href="about_page.html" target="_self">ABOUT</a></li>
+                <li id="login"><a href="login.html" target="_self">LOGIN/SIGN UP</a></li>
+            </ul>
+        </nav>
+    </div>
+    
+<main>
+    <h2>ABOUT</h2>
+    <div id="content">
+        <img src="images/welcome%20(2).png" alt="welcome">
+        <p class="a"> What is Bank Of Ontario? </p>
+        <p class="b">
+            Incorporated in 1832, the bank has established itself as Canada's<br> most international bank through extensive operations throughout<br>Latin America, the Caribbean, Central America and parts of Asia.<br> It is also known as “Canada's gold bank” because of its dominant<br> position in bullion trading.
+        </p><br>
+        <p class="a">Why Bank Of Ontario?</p>
+        <p class="b">
+            As Canada's third-largest bank, Bank Of Ontario is a very popular<br> banking institution across the country as well, thanks to its broad<br> range of credit cards, chequing accounts, and savings accounts that<br> include special offerings for youth, students, and seniors.
+        </p><br>
+        <p class="a">How Bank Of Ontario is Unique?</p>
+        <p class="b">
+            Bank Of Ontario is unique among Canada's big five banks in that<br> it has substantial exposure to emerging markets through its international<br> banking segment. This segment has generated momentum with record<br> results in the first quarter.These results were underpinned by growth<br> in its key Pacific Alliance countries--Chile,Columbia,Mexico and Peru.
+        </p>
+    </div>      
+</main>
+
+<footer>
+    <p>Subscribe to get latest updates and weekly newsletter</p>
+    <input id="mail" type="text" placeholder="EMAIL ADDRESS" name="mail" required>&nbsp;&nbsp;
+    <input id="subscribe" type="submit" value="SUBSCRIBE">
+    <section class="bottom">
+      <h3>Bank Of Ontario</h3>
+       <p>At BFO, we are in business to help our clients, employees and shareholders achieve what is important to them.<br><br> Our ability to create value for all BFO stakeholders is driven by a business culture based on common values:<br><br> Trust<br> Teamwork<br> Accountability<br><br>
+            All you have to do is sign-up and start exploring!</p><br><br>
+      <a href="contact.html">Contact Us</a>
+      <a href="faq.html">FAQ</a>&nbsp;&nbsp;
+      <a href="terms.html">Terms&Conditions</a>&nbsp;&nbsp;
+      <a href="privacy.html">Privacy Policy</a>&nbsp;&nbsp;
+    </section>
+</footer>
+
+</body>
+    
+</html>
