ea6bfd75bbbbeb843618ec849b33b854c2bc8dc6
drahul41 <rdivakaruni8866@conestogac.on.ca>
2020-10-30 00:26:53 -0400

    Terms page creation

---
+++ terms.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<!-- Title for the page -->
+<title> Terms & Conditions - Bank Of Ontario</title>
+<!--Meta information-->
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<meta name="description" content="This is a Terms Page"/>
+<meta name="keywords" content="about Bank Of Ontario"/>
+<link rel="stylesheet" href="css/about.css"/>
+<link rel="shortcut icon" href="images/logoB.png"/>
+
+</head>
+
+<body>
+<?php include 'header.php';?>
+    
+<main>
+    <h2>Terms and Conditions</h2>
+    <div id="content">
+        
+       
+        <p class="b"> 1] 
+        You can have up to twenty (20) Canadian or U.S. Dollar Primary Chequing, Premium .Rate Savings or Interest Chequing Accounts (for existing clients just) barring Smart Saver Accounts and Savings Builder Account under one (1) Plan. One (1) account must be assigned as the lead account. The lead account is the one you assign to pay any expenses needed by your Bank Plan, for instance, your month to month Plan charges and exchange expenses. On the off chance that you select a U.S. Dollar Account as the lead account, all Plan and exchange expenses will be charged in U.S. dollars. All records secured by the Plan are dependent upon the month to month exchange limit(s) for the chose Plan.<br> 
+        The Practical Plan permits you to have one (1) Canadian or U.S. Dollar Primary Chequing Account or Interest Chequing Account (existing clients) and one (1) Premium Rate Savings Account under the Plan. All other Bank Plans permit you to have up to an aggregate of twenty (20) accounts in Canadian or U.S. Dollars, any mix of Primary Chequing Accounts, Premium Rate Savings Accounts, and Interest Chequing Accounts (existing clients just) under the Plan</p><br>
+       
+        <p class="b"> 2]
+        Bill installments, Pre-Authorized Payments, retail location exchanges, Interac e-Transfer and checks are not allowed on this record </p><br>   
+        
+        <p class="b"> 3]
+        Interest is determined on the every day account shutting balance and paid month to month on the last business day of the month. Interest is paid in a similar money as the record. Loan fees are determined on a for each annum premise. Financing costs and equilibrium levels are liable to change without early notification. Loan costs are accessible online, in plain view at all BOO branches where store accounts are kept. </p>       
+        
+        <p class="b"> 4] 
+        The month to month Bank Plan expense (barring the AIR MILES Plan and Practical Plan) can be disposed of by keeping up the base month to month balance demonstrated consistently in a Primary Chequing account, which has been assigned as the lead represent your Bank Plan. The lead account is the one you assign to pay any expenses needed by your Bank Plan for instance, month to month Plan charges and exchange charges. You are answerable for all exchange, administration and item charges excluded from Bank Plan.
+        </p><br>    
+        <p class= "b"> 5]
+        There is no charge to get to BOO Mobile Banking. Exchanges finished through BOO Mobile Banking might be dependent upon exchange charges relying upon your financial balance or Bank Plan. Your portable transporter or specialist co-op may charge expenses when you access Mobile Banking. Check with your portable transporter or equipment supplier in the event that you have inquiries regarding your particular gadget.
+        </p><br>
+        <p class= "b"> 6]
+        The base loan cost applies to the whole equilibrium in a Savings Builder ("Account"). You will procure reward interest on your Account total up to $250,000.00, every month wherein you increment your month to month Account surplus by $200.00 or more. To decide whether you are qualified for reward interest in a month, we will figure the distinction between the end balance on the last business day of the earlier month and the end balance on the last business day of the current month (barring base interest from the current month, reward interest credited from the earlier month and any withdrawal or move charges); that distinction must be in any event $200.00. The base interest is determined daily, shutting balance and paid month to month on the last business day of the month. The reward interest is determined daily, shutting balance and paid month to month on the second business day of the next month. The reward interest is determined from the month's primary business day, paying little heed to which day you qualified for reward interest. The base loan cost and reward financing cost each might be changed whenever without notice. Current loan costs are accessible at BOO.
+        </p><br>
+        <p class= "b"> 7]
+        Subject to the terms and states of your understanding of banking administrations. For complete subtleties of your revealing commitments and duties: individual financial clients ought to allude to the Electronic Banking Services Agreement a piece of the Agreements, Bank Plans and Fees for Everyday Banking; and business banking clients ought to allude to the BOO Debit Card for Business and Telephone Banking/Online Banking segment of the Agreement for Business Banking. The two arrangements are accessible online.
+        </p><br>
+
+    </div>      
+</main>
+
+<?php include 'footer.php';?>
+
+</body>
+    
+</html>
+
