5854995236607108898b4caf51537165cfda06f1
Akash Basava <saiakash81197@gmail.com>
2020-10-30 10:47:37 -0400

    changes in the form for signup page -akash

--- signup.php
+++ signup.php
@@ -16,12 +16,17 @@ form {
 
 /* Full-width inputs */
 .inputblocks {
-  width: 30%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
+  width: 40%; 
+  padding:12px 20px;
+  margin: 8px 8px;
+  display: block;
+  border: 2px solid #ccc;
   box-sizing: border-box;
+  text-align:center;
+  
+
+  
+  
 }
 
 /* Set a style for all buttons */
@@ -65,18 +70,41 @@ span.psw {
     <main>
     <?php include 'headerimage.php';?>
         <div id="content">
+        <center>
         <form action="action_page.php" method="post">
     <div class="container">
+
+    <label for="firstname"><b>Firstname</b></label>
+    <input type="text" class = "inputblocks" placeholder="Enter Firstname" name="firstname" required><br>
+
+    <label for="lastname"><b>Lastname</b></label>
+    <input type="text" class = "inputblocks" placeholder="Enter Lastname" name="lastname" required><br>
+
+    <label for="dateofbirth"><b>Dateofbirth</b></label>
+    <input type="date" class = "inputblocks" placeholder="Enter Dateofbirth" name="dateofbirth" required><br>
+
+    <label for="sin"><b>Sin</b></label>
+    <input type="number" class = "inputblocks" placeholder="Enter your Sin " name="sin" required><br>
+
+    <label for="phone"><b>Phone</b></label>
+    <input type="tel" class = "inputblocks" placeholder="Enter Phone" name="phone" required><br>
+
+    <label for="address"><b>Address</b></label>
+    <input type="text" class = "inputblocks" placeholder="Enter Address" name="address" required><br>
+
+    <label for="username"><b>Username</b></label>
+    <input type="text" class = "inputblocks" placeholder="Enter Username" name="username" required><br>
+
     <label for="email"><b>Email</b></label>
     <input type="text" class = "inputblocks" placeholder="Enter Email" name="email" required><br>
 
     <label for="psw"><b>Password</b></label>
     <input type="password" class = "inputblocks" placeholder="Enter Password" name="psw" required><br>
 
-    <label for="psw-repeat"><b>Repeat Password</b></label>
+    <label for="psw-repeat"><b>Confirm Password</b></label>
     <input type="password" class = "inputblocks" placeholder="Enter Password" name="psw-repeat" required><br>
 
-    <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p><br>
+    <h4>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</h4><br>
 
     <button type="submit">Signup</button><br>
     <button type="button">Cancel</button>
@@ -85,6 +113,7 @@ span.psw {
 
   
 </form>
+</center>
     </main>
     
     <?php include 'footer.php';?>
