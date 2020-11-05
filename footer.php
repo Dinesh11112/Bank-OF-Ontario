3a55b5736348b20a17148237ba1cf3711ea64856
Dinesh11112 <dineshboby07@gmail.com>
2020-11-05 18:48:46 -0500

    added minor jquery stuff in footer.php. added php code to enter the formdata to the DB table.

--- footer.php
+++ footer.php
@@ -1,10 +1,14 @@
 <?php
-
+echo "<script
+src='https://code.jquery.com/jquery-3.5.1.min.js'
+integrity='sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0='
+crossorigin='anonymous'></script>";
 echo"
 <footer>
+<form method='post'>
 <p>Subscribe to get latest updates and weekly newsletter</p>
 <input id='mail' type='text' placeholder='EMAIL ADDRESS' name='mail' required>&nbsp;&nbsp;
-<input id='subscribe' type='submit' value='SUBSCRIBE'>
+<input id='subscribe' type='submit' value='SUBSCRIBE'></form>
 <section class='bottom'>
   <h2>Bank Of Ontario</h2>
   <p>At BFO, we are in business to help our clients, employees and shareholders achieve what is important to them.<br><br> Our ability to create value for all BFO stakeholders is driven by a business culture based on common values:<br><br> Trust<br> Teamwork<br> Accountability<br><br>
@@ -15,5 +19,11 @@ echo"
   <a href='privacy.php'>Privacy Policy</a>&nbsp;&nbsp;
 </section>
 </footer>
-"
+";
+/*
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+  $mailid = $_POST['mail'];
+
+  echo "<script>alert('hellooo $mailid');</script>";
+}*/
 ?>
