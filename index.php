19e8ecae641725ec24af7005c4a46d10ecee0da8
DESKTOP-OBCANBD\user <saiakash81197@gmail.com>
2020-11-22 22:39:51 -0500

    content change

--- index.php
+++ index.php
@@ -15,13 +15,13 @@
     <?php include 'headerimage.php';?>
         <div id="content">
             <img src="images/home.png">
-            <h2>WHY BANK OF ONTARIO</h2><br>s
+            <h2>WHY BANK OF ONTARIO</h2><br>
             <p>A Canadian-headquartered bank with focus on high-quality growth markets in the Americas. We are a top-five universal bank in each of our core markets, and a top-15 wholesale bank in the U.S., delivering superior advice and services to help our customers get ahead.
             We are serving customers and driving growth through our commitment to three pillars:<b>
 <br><br>Focus on the Americas :</b>Focused on six core markets and supported by leading expertise allows us to prioritize our investment for long-term growth.<br><br><b>Customer First:</b>By putting our customers first, we’re helping them be better off today and tomorrow.<br><br><b>Winning Team:</b>A purpose-driven team, committed to results in an inclusive and high-performance culture.
            <br><br> <b>$0 Monthly Banking Fees:</b>Pay no monthly banking fees, upon meeting the eligibility criteria, and get unlimited day-to-day banking.
            <br><br><b>Easy banking Access:</b>Send money with $0 wire transfer fees7. Easily view & transfer money online between accounts.
-            <br><br><b>Simple and Secure Online Banking:</b>View online statements, pay bills securely,and much more.    
+            <br><br><b>Simple and Secure Online Banking:</b>View online statements, pay bills securely,and much more.  
         </p>
         </div>
         <section>
